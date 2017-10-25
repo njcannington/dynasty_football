3b66ae55be7b5174f4bfcdfcdb1e815531e30ba0
Nic <njcannington@gmail.com>
2017-10-25 12:19:45 -0400

    add unit tests (using phpunit)

--- autoload.php
+++ autoload.php
@@ -6,3 +6,5 @@ const ROOT = __DIR__;
 const VIEWS = ROOT."/app/views";
 const CONTROLLERS = ROOT."/app/controllers";
 const MODELS = ROOT."/app/models";
+
+require_once(ROOT."/vendor/autoload.php");//used for phpunit
---
+++ tests/fleaflicker/leaguetest.php
@@ -0,0 +1,31 @@
+<?php
+namespace tests\fleaflicker;
+
+$_SESSION["env"] = "dev";
+
+use Lib\Scraper\Fleaflicker;
+
+class LeagueTests extends \PHPUnit_Framework_TestCase
+{
+    private $league;
+
+    public function setUp()
+    {
+        $this->league = new Fleaflicker\League();
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testGetTeams()
+    {
+        $teams = $this->league->getTeams();
+        $this->assertGreaterThan(0, count($teams), "does not return array");
+        foreach ($teams as $team) {
+            $this->assertTrue(is_string($team["name"]), "does not return a string");
+            $this->assertTrue(is_string($team["id"]), "does not return a string");
+            $this->assertTrue(is_string($team["owner"]), "does not return a string");
+        }
+    }
+}
