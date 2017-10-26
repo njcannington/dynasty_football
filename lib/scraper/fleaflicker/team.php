9a17ab4d724ec0cf8eb79b77ea13a1bd14ff688c
Nic <njcannington@gmail.com>
2017-10-26 13:24:44 -0400

    refactor scraper
    Some of the methods in `Scraper` class were a bit bulky. I broke out some into separate methods.

--- lib/scraper/fleaflicker/team.php
+++ lib/scraper/fleaflicker/team.php
@@ -1,30 +1,31 @@
 <?php
 namespace Lib\Scraper\Fleaflicker;
 
-use Lib\Scraper\Fleaflicker\Fleaflicker;
+use Lib\Scraper\Scraper;
 use Lib\Config\Config;
 
-class Team extends Fleaflicker
+class Team extends Scraper
 {
+    protected $cookie = '';
+
     const XPATH_STARTER_DATA = "//table[@id='table_0']/tr/td[1]/div[@class='player']/div[@class='player-name']/a";
-    protected $players = [];
 
     public function __construct($team_id)
     {
-        $config = Config::getInstance();
-        $this->url = parent::BASE_URL.$config["ff"]["type"]."/leagues/".$config["ff"]["league"]."/teams/".$team_id;
+        $league_type = Config::getInstance()["ff"]["type"];
+        $league_id = Config::getInstance()["ff"]["league"];
+        $this->url = "https://www.fleaflicker.com/".$league_type."/leagues/".$league_id."/teams/".$team_id;
         parent::__construct();
-        $this->setPlayers();
     }
-
-    private function setPlayers()
-    {
-        $this->players = $this->extractTextContent(self::XPATH_STARTER_DATA);
-    }
-
 
     public function getPlayers()
     {
-        return $this->players;
+        try {
+            $players = $this->getTextContent(self::XPATH_STARTER_DATA);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            return;
+        }
+        return $players;
     }
 }
