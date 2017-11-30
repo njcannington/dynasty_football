802d1052a899e2c458a0a31f2be707ed4faf1dd9
Nic <njcannington@gmail.com>
2017-11-30 09:35:21 -0500

    move js to web directory and update readme

--- web/js/javascript.js
+++ web/js/javascript.js
@@ -1,27 +1,24 @@
 function sortTable(table, col, reverse) {
-    var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
-        tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
+    var tb = table.tBodies[0], 
+        tr = Array.prototype.slice.call(tb.rows, 0),
         i;
     reverse = -((+reverse) || -1);
-    
-    tr = tr.sort(function (a, b) { // sort rows
-        
-        
+    tr = tr.sort(function (a, b) {
         if(!isNaN(a.cells[col].textContent) && !isNaN(b.cells[col].textContent))
         return reverse * ((+a.cells[col].textContent) - (+b.cells[col].textContent))
-       return reverse // `-1 *` if want opposite order
-            * (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
+       return reverse
+            * (a.cells[col].textContent.trim()
                 .localeCompare(b.cells[col].textContent.trim())
                );
     });
-    for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
+    for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]);
 }
 
 function makeSortable(table) {
     var th = table.tHead, i;
     th && (th = th.rows[0]) && (th = th.cells);
     if (th) i = th.length;
-    else return; // if no `<thead>` then do nothing
+    else return;
     while (--i >= 0) (function (i) {
         var dir = 1;
         th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
