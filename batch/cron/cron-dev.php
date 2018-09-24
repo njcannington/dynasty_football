4f782fa91aa5e0f9ba5244608ac0d619893ccd7d
Nic <njcannington@gmail.com>
2018-09-24 15:06:30 -0400

    update cron jobs to use create() as static method

--- batch/cron/cron-dev.php
+++ batch/cron/cron-dev.php
@@ -9,24 +9,22 @@ $_SESSION["env"] = "dev";
 use App\Lib\Scraper\DynastyLeagueFootball;
 use App\Models;
 
-$rankings = new Models\Ranking();
-
 $qb = new DynastyLeagueFootball\Quarterback();
 foreach ($qb->getRankings() as $data) {
-    $rankings->create("QB", $data);
+    Models\Ranking::create("QB", $data);
 }
 
 $rb = new DynastyLeagueFootball\Runningback();
 foreach ($rb->getRankings() as $data) {
-    $rankings->create("RB", $data);
+    Models\Ranking::create("RB", $data);
 }
 
 $te = new DynastyLeagueFootball\Tightend();
 foreach ($te->getRankings() as $data) {
-    $rankings->create("TE", $data);
+    Models\Ranking::create("TE", $data);
 }
 
 $wr = new DynastyLeagueFootball\Widereceiver();
 foreach ($wr->getRankings() as $data) {
-    $rankings->create("WR", $data);
+    Models\Ranking::create("WR", $data);
 }
